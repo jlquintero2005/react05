55e8f3e085924dd406f2e9c56f7d99fe59a233d5
Jorge Luis <jlquintero2611@gmail.com>
2022-06-30 15:22:40 -1100

    version 5

---
+++ docs/precache-manifest.0259ef5eb9206fcbb8cb0e8708e151a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c74ee7e2d5fdfb89036d",
+    "url": "/static/js/main.c74ee7e2.chunk.js"
+  },
+  {
+    "revision": "5f383ccaaa84446c2303",
+    "url": "/static/js/1.5f383cca.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "133fdfcb15c7232a5fd529b4aab31e30",
+    "url": "/index.html"
+  }
+];
